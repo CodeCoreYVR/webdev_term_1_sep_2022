ed78564d9d423cb1b654319d36b1738dffd7a322
Caleb Faulhaber <caleb@codecore.ca>
2022-11-15 07:40:23 +0000

    js_objects added labs

---
+++ js_objects/labs/digging_into_objects_and_arrays.js
@@ -0,0 +1,36 @@
+var myCrazyObject = {
+"name": "A ridiculous object",
+"some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
+"random animal": "Banana Shark"
+};
+
+console.log(myCrazyObject['some array'][2].number)
+
+
+// let animals = {
+//   'name': "shark",
+//   'teeth': "too many",
+//   'food': [{'fish': 'tuna'}, 'squid', 'surfboards']
+// }
+
+
+
+
+
+// [Lab] Digging Into Objects and Arrays
+
+// Say you had the following object:
+
+// var myCrazyObject = {
+
+// "name": "A ridiculous object",
+
+// "some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
+
+// "random animal": "Banana Shark"
+
+// };
+
+// How would you get the number 123 out of this object using one line of JavaScript? Try it out in the
+
+// console to see if you’re right
