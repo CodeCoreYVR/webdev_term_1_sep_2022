d29bbdb41408212d29ab1f25855c00f6dd9c9493
Caleb Faulhaber <caleb@codecore.ca>
2023-01-05 20:10:18 +0000

    CSS Animations and SVG: Create an Animation

--- css_animation_and_svg/labs/javascript/index.js
+++ css_animation_and_svg/labs/javascript/index.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
       left: (event.pageX - 100)
     });
   });
+
+  // create an animation - h1 tag should move in square pattern
+  const coords = [[0,50], [200,50], [200,250], [0,250]]
+  let state = 0;
+  let clicks = 0;
+
+  const intervalID = setInterval(function(){
+    $("#square").animate({
+      marginLeft: coords[state][0],
+      marginTop: coords[state][1]
+    })
+    state < 3 ? state++ : state = 0;
+  }, 1100 - 100*clicks)
+
 });
